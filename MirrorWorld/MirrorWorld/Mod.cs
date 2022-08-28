dfa0622e118d44f826b3da75192dd5f048bf1027
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-08-28 16:59:43 -0400

    MirrorWorld mod project

---
+++ MirrorWorld/MirrorWorld/Mod.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+using UnityEngine;
+
+namespace MirrorWorld
+{
+    [QModCore]
+    public static class Mod
+    {
+        public static Assembly modAssembly = Assembly.GetExecutingAssembly();
+
+        [QModPatch]
+        public static void Entry()
+        {
+            var harmony = new Harmony("Lee23.MirrorWorld");
+            harmony.PatchAll(modAssembly);
+        }
+    }
+}
