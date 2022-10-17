4f10c56fef11ab841b9db5eaecb080d6e2d6ae40
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-10-16 21:07:02 -0400

    upload MonkeySayMonkeyGet source code

---
+++ MonkeySayMonkeyGet/MonkeySayMonkeyGet/Mono/CreatureFlinch.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MonkeySayMonkeyGet.Mono
+{
+    public class CreatureFlinch : MonoBehaviour
+    {
+        public Creature creature;
+
+        private void Update()
+        {
+            if (creature)
+            {
+                creature.GetAnimator().SetFloat("flinch", 1f);
+            }
+        }
+    }
+}
---
+++ MonkeySayMonkeyGet/MonkeySayMonkeyGet/VoiceCommands/Priority.cs
@@ -0,0 +1,11 @@
+﻿namespace MonkeySayMonkeyGet.VoiceCommands
+{
+    public enum Priority
+    {
+        None = 0,
+        Low = 256,
+        Medium = 512,
+        High = 1024,
+        Highest = 2048
+    }
+}
