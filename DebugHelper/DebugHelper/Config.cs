19af1d7eb52198ada4064524e68dd25750dd18eb
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-02 22:01:46 -0400

    update config organization

--- DebugHelper/DebugHelper/Config.cs
+++ DebugHelper/DebugHelper/Config.cs
@@ -7,16 +7,16 @@ namespace DebugHelper
     [Menu("DebugHelper")]
     public class Config : ConfigFile
     {
-        [Toggle("Debug Wire Overlay (Enable?)", Tooltip = "If checked, the output of Debug.DrawLine and Debug.DrawRay will be displayed on the screen.")]
+        [Toggle("Debug Wire Overlay (Enable?)", Tooltip = "If checked, the output of Debug.DrawLine and Debug.DrawRay will be displayed on the screen.\nTHIS IS AN EXPERIMENTAL FEATURE.")]
         public bool DebugOverlayEnabled = false;
-        [Slider("Debug Wire Overlay (Resolution)", DefaultValue = 50, Min = 10, Max = 100, Tooltip = "Percent of resolution of the Debug Wire Overlay.")]
-        public float DebugOverlayResolutionPercent = 50;
+        [Slider("Debug Wire Overlay (Resolution)", DefaultValue = 50, Min = 10, Max = 100, Tooltip = "Percent of resolution of the Debug Wire Overlay.\nTHIS IS AN EXPERIMENTAL FEATURE. VALUES LESS THAN 100 WILL BE INACCURATE.")]
+        public float DebugOverlayResolutionPercent = 100;
 
-        [Slider("Debug Icons (Scale)", DefaultValue = 50f, Min = 0f, Max = 100f)]
+        [Slider("Debug Icons (Scale)", DefaultValue = 50f, Min = 0f, Max = 100f, Tooltip = "Percent of scale of the Debug Icons that overlay the screen.")]
         public float DebugIconScalePercent = 50f;
-        [Toggle("Debug Icons (3D?)")]
+        [Toggle("Debug Icons (3D?)", Tooltip = "Whether Debug Icons will decrease in size with distance or not.")]
         public bool DebugIconsAre3D = true;
-        [Slider("Debug Icons (Search range)", DefaultValue = 35f, Min = -1f, Max = 150f, Tooltip = "Values less than 0 count as infinity. High values can be VERY slow.")]
+        [Slider("Debug Icons (Search range)", DefaultValue = 35f, Min = -1f, Max = 150f, Tooltip = "Components in this range will be debugged. Values less than 0 count as infinity. High values can be VERY slow.")]
         public float DebugRange = 35f;
         [Slider("Debug Icons (Update interval)", DefaultValue = 1f, Min = 1f, Max = 10f, Tooltip = "The number of seconds between debug renderers being automatically regenerated.\nIncrease this value if the debug systems are affecting framerate.")]
         public float DebugUpdateInterval = 1f;
