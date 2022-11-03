b5bf64460e3da84219a2f1f18b62cc5c9996bb9c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-02 22:01:30 -0400

    new player cmds

--- DebugHelper/DebugHelper/Commands/PlayerCommands.cs
+++ DebugHelper/DebugHelper/Commands/PlayerCommands.cs
@@ -1,4 +1,5 @@
 ﻿using SMLHelper.V2.Commands;
+using UnityEngine;
 
 namespace DebugHelper.Commands
 {
@@ -32,6 +33,37 @@ namespace DebugHelper.Commands
         public static void PlayerAnimatorFloat(string parameter, float value)
         {
             Player.main.playerAnimator.SetFloat(parameter, value);
+        }
+
+        [ConsoleCommand("copyposition")]
+        public static void CopyPosition()
+        {
+            CopyPositionInternal(false);
+        }
+
+        [ConsoleCommand("copyrayposition")]
+        public static void CopyRayPosition()
+        {
+            CopyPositionInternal(true);
+        }
+
+        private static void CopyPositionInternal(bool raycast)
+        {
+            Vector3 position;
+            var cam = MainCamera.camera.transform;
+            if (raycast && Targeting.GetTarget(Player.main.gameObject, 2000f, out GameObject target, out float distance))
+            {
+                position = cam.position + cam.forward * distance;
+                ErrorMessage.AddMessage("Copying the position of a raycast from the crosshair to clipboard.");
+            }
+            else
+            {
+                ErrorMessage.AddMessage("Copying the position of the player camera to clipboard.");
+                position = cam.position;
+            }
+            var formatted = Helpers.FormatVector3Constructor(position, 2);
+            ErrorMessage.AddMessage("Formatted constructor: " + formatted);
+            GUIUtility.systemCopyBuffer = formatted;
         }
     }
 }
