9a897dbbf9239f2033bcc876f0dfed3b5b470083
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-06 18:41:28 -0500

    Update Main.cs

--- AggressiveFauna/AggressiveFauna/Main.cs
+++ AggressiveFauna/AggressiveFauna/Main.cs
@@ -1,8 +1,10 @@
 ﻿using QModManager.API;
 using QModManager.API.ModLoading;
 using HarmonyLib;
+using System.IO;
 using System.Reflection;
 using SMLHelper.V2.Handlers;
+using UnityEngine;
 
 /* Light version of DeathRun, focusing on creature aggression.
  * Credit to Cattlesquat for original code
@@ -14,13 +16,22 @@ namespace AggressiveFauna
     public static class Main
     {
         internal static Config config;
+        internal static Assembly assembly = Assembly.GetExecutingAssembly();
+        internal static AssetBundle bundle;
 
         [QModPatch]
         public static void Entry()
         {
+            bundle = LoadAssetBundleFromAssetsFolder(assembly, "aggressivefauna");
             config = OptionsPanelHandler.RegisterModOptions<Config>();
             var harmony = new Harmony("Lee23.AggressiveFauna");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            harmony.PatchAll(assembly);
+            ModAudio.PatchAudio();
+        }
+
+        private static AssetBundle LoadAssetBundleFromAssetsFolder(Assembly modAssembly, string assetsFileName)
+        {
+            return AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(modAssembly.Location), "Assets", assetsFileName));
         }
     }
 }
