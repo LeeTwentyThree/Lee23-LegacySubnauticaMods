e836622167ba4d612a1dc73e6a306d626482d3ff
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-05 20:08:36 -0400

    FOVScale config

--- AggressiveFauna/AggressiveFauna/Config.cs
+++ AggressiveFauna/AggressiveFauna/Config.cs
@@ -12,15 +12,15 @@ namespace AggressiveFauna
         public bool AffectDaytime = true;
         [Toggle("Changes apply during night", Tooltip = "If disabled, the settings below will not have an effect during nighttime.")]
         public bool AffectNighttime = true;
-        [Slider("Detection radius multiplier", DefaultValue = 1f, Min = 0.0f, Max = 5f, Step = 0.1f, Tooltip = "The scale factor of the detection range of aggressive creatures.\nUnmodded value: 1x")]
+        [Slider("Detection radius multiplier", DefaultValue = 1f, Min = 1f, Max = 5f, Step = 0.1f, Tooltip = "The scale factor of the detection range of aggressive creatures.\nUnmodded value: 1x")]
         public float DetectionRadiusMultiplier = 3f;
         [Toggle("Detection through terrain", Tooltip = "If enabled, creatures will be able to detect you through walls.\nUnmodded value: False")]
         public bool CanSeeThroughTerrain = false;
         [Toggle("Detection through bases", Tooltip = "If enabled, creatures will be able to detect you while you're inside a base. They still can not attack the base.\nUnmodded value: False")]
         public bool CanSeeThroughBases = false;
-        [Slider("Player prioritization", DefaultValue = 1f, Min = 0.0f, Max = 5f, Step = 0.1f, Tooltip = "The higher this value, the more creatures will be more likely to attack you over other targets in range.\nUnmodded value: 1x")]
+        [Slider("Player prioritization", DefaultValue = 1f, Min = 1f, Max = 5f, Step = 0.1f, Tooltip = "The higher this value, the more creatures will be more likely to attack you over other targets in range.\nUnmodded value: 1x")]
         public float PlayerPrioritization = 3f;
-        [Slider("Aggression multiplier", DefaultValue = 1f, Min = 0.0f, Max = 10f, Step = 0.1f, Tooltip = "General aggression multiplier. Scales the minimum aggression required for attacking.\nUnmodded value: 1x")]
+        [Slider("Aggression multiplier", DefaultValue = 1f, Min = 1f, Max = 10f, Step = 0.1f, Tooltip = "General aggression multiplier. Scales the minimum aggression required for attacking.\nUnmodded value: 1x")]
         public float AggressionMultiplier = 3f;
         [Toggle("No fleeing", Tooltip = "If enabled, creatures will not flee when taking damage.\nUnmodded value: False")]
         public bool DisableFleeing = true;
@@ -32,5 +32,7 @@ namespace AggressiveFauna
         public bool DisableAttackDelay = true;
         [Toggle("Attack unoccupied vehicles", Tooltip = "If enabled, creatures will be able to target vehicles that are not being piloted.\nUnmodded value: False")]
         public bool AttackUnoccupiedVehicles = true;
+        [Slider("FOV multiplier", DefaultValue = 1f, Min = 1f, Max = 4f, Step = 0.1f, Tooltip = "Higher values allow creatures to see you more easily.\nUnmodded value: 1x")]
+        public float FOVScale = 3f;
     }
 }
