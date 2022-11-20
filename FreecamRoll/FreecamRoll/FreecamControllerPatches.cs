a253be266da59dc0cef1c5a18b229d8fd63da093
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-20 17:37:07 -0500

    fix ryley breaking his neck

--- FreecamRoll/FreecamRoll/FreecamControllerPatches.cs
+++ FreecamRoll/FreecamRoll/FreecamControllerPatches.cs
@@ -7,11 +7,20 @@ namespace FreecamRoll
     internal static class FreecamControllerPatches
     {
         [HarmonyPatch(nameof(FreecamController.Update))]
+        [HarmonyPostfix()]
         internal static void FreecamControllerUpdatePostfix(FreecamController __instance)
         {
+            if (!__instance.inputDummy.activeSelf)
+            {
+                return;
+            }
+            if (!__instance.mode || !UWE.Utils.lockCursor)
+            {
+                return;
+            }
             float rollDelta = 0;
-            if (Input.GetKey(KeyCode.Q)) rollDelta = -1;
-            else if (Input.GetKey(KeyCode.E)) rollDelta = 1;
+            if (Input.GetKey(KeyCode.Q)) rollDelta = 1;
+            else if (Input.GetKey(KeyCode.E)) rollDelta = -1;
             rollDelta *= Main.sensitivity;
             rollDelta *= Time.deltaTime;
             __instance.tr.localEulerAngles += new Vector3(0, 0, rollDelta);
