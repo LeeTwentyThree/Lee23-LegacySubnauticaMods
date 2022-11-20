0d7615506825036a424f60b699ab9115ebd59bbf
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-11-20 17:21:19 -0500

    FreecamRoll mod

---
+++ FreecamRoll/FreecamRoll/FreecamControllerPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace FreecamRoll
+{
+    [HarmonyPatch(typeof(FreecamController))]
+    internal static class FreecamControllerPatches
+    {
+        [HarmonyPatch(nameof(FreecamController.Update))]
+        internal static void FreecamControllerUpdatePostfix(FreecamController __instance)
+        {
+            float rollDelta = 0;
+            if (Input.GetKey(KeyCode.Q)) rollDelta = -1;
+            else if (Input.GetKey(KeyCode.E)) rollDelta = 1;
+            rollDelta *= Main.sensitivity;
+            rollDelta *= Time.deltaTime;
+            __instance.tr.localEulerAngles += new Vector3(0, 0, rollDelta);
+        }
+    }
+}
