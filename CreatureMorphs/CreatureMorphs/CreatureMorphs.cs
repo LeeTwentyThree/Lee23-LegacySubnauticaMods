eaf301cea8ca9a702310df7a0a03b1c4de2cf7ab
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-12-03 19:04:54 -0500

    set up CreatureMorphs

---
+++ CreatureMorphs/CreatureMorphs/CreatureMorphs.cs
@@ -0,0 +1,14 @@
+﻿using QModManager.API.ModLoading;
+
+namespace CreatureMorphs
+{
+    [QModCore]
+    public static class Main
+    {
+        [QModPatch]
+        public static void Entry()
+        {
+
+        }
+    }
+}
